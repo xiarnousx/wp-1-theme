546d6e18575ec9327f02a4f961382252f1f26c04
Ihab Arnous <ihabarnus@gmail.com>
2024-06-12 16:09:52 +0300

    WIP WP-2

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="<?php bloginfo('charset'); ?>" />
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -13,13 +15,19 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-            <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <?php  
+              // wp_nav_menu([
+              //   'theme_location' => 'headerMenuLocation'
+              // ]); 
+            ?>
+           <ul>
+              <li class="<?php echo is_page('about-us')? 'current-menu-item':''; ?>"><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
+
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
--- page.php
+++ page.php
@@ -14,24 +14,37 @@ while(have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <?php if($id = wp_get_post_parent_id(get_the_ID())): ?>
+        <?php $id = wp_get_post_parent_id(get_the_ID()); ?>
+        <?php if($id): ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
             <a class="metabox__blog-home-link" href="<?php echo get_permalink($id); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($id) ?></a> <span class="metabox__main"><?php the_title(); ?></span>
             </p>
         </div>
       <?php endif; ?>
-
+      <?php
+        $subs = get_pages([
+          'child_of' => get_the_ID(),
+        ]) 
+      ?>
+      <?php if ($id || $subs): ?>
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($id); ?>"><?php echo get_the_title($id); ?></a></h2>
         <ul class="min-list">
             <?php 
-                wp_list_pages([])
+                $childOf = $id ? $id: get_the_ID();
+                wp_list_pages([
+                  'title_li' => null,
+                  'child_of' => $childOf,
+                  'sort_column' => 'menu_order'
+                ]);
             ?>
+            <!--
           <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+              -->
         </ul>
       </div>
+      <?php endif; ?>
 
       <div class="generic-content"><?php the_content(); ?></div>
     </div>
