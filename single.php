6ee8f8a43b5831f7b7c8584512f9755d8e9dd899
Ihab Arnous <ihabarnus@gmail.com>
2024-06-12 12:02:50 +0300

    WIP WP-1

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+while(have_posts()) {
+    the_post();
+    ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+        <hr />
+    <?php
+}
+get_footer();
